41f8c0a2c082036d1308fb081b114a4d9c2b576b
huska0707 <hussaink001haled@gmail.com>
2024-05-23 20:39:28 -0500

    React Node Web Scraper interface

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>Welcome</div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://localhost:8000/api/vi/items")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error", error));
+  });
+  return (
+    <div>
+      <h1>React Node Web Scraper</h1>
+      {data &&
+        data.map((item, index) => (
+          <div key={item}>
+            <h4>{item.name}</h4>
+            <img src={item.img} alt={item.name} width={100} height={100} />
+            <p>{item.price}</p>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default App;
